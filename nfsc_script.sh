8b5cdeb27a65f643eb101909b7e4296dd2a7a270
SaboRV <sabirzyanov.r@gmail.com>
2024-01-11 00:36:56 +0500

    .

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -5,7 +5,6 @@ apt-get install -y mc
 mkdir -p /mnt/nfs_clientshare
 echo "192.168.56.20:/srv/share/ /mnt/nfs_clientshare nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
 mount -a
-sudo ufw enable
 ufw allow 2222
 ufw allow 22
 yes | sudo ufw enable
